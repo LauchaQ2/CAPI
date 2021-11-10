ffc0478b9db5d15edc277b3604d73384763540a8
LauchaQ2 <89219479+LauchaQ2@users.noreply.github.com>
2021-11-10 15:07:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+ScrollReveal().reveal('#dataBanner1', { 
+    delay: 600, 
+    distance: '50px',
+    duration: 1200
+});
+ScrollReveal().reveal('#dataBanner2', { 
+    delay: 800, 
+    distance: '50px',
+    duration: 1200
+});
+ScrollReveal().reveal('#dataBanner3', { 
+    delay: 1000, 
+    distance: '50px',
+    duration: 1200
+});
+
+ScrollReveal().reveal('#header', { 
+    delay: 300, 
+    distance: '100px',
+    origin: 'top',
+    duration: 1000
+});
+
+ScrollReveal().reveal('#patologia1', { 
+    delay: 600, 
+    distance: '50px',
+    duration: 1500,
+    origin: 'left'
+});
+ScrollReveal().reveal('#patologia2', { 
+    delay: 600, 
+    distance: '50px',
+    duration: 1500,
+    origin: 'top'
+});
+ScrollReveal().reveal('#patologia3', { 
+    delay: 600, 
+    distance: '50px',
+    duration: 1500,
+    origin: 'right'
+});
+ScrollReveal().reveal('#titlePathology', { 
+    delay: 400, 
+    distance: '50px',
+    duration: 1200,
+    origin: 'top'
+});
+ScrollReveal().reveal('#titleUs', { 
+    delay: 400, 
+    distance: '50px',
+    duration: 1200,
+    origin: 'top'
+});
+ScrollReveal().reveal('#us1', { 
+    delay: 600, 
+    distance: '50px',
+    duration: 1500,
+    origin: 'left'
+});
+ScrollReveal().reveal('#us2', { 
+    delay: 600, 
+    distance: '50px',
+    duration: 1500,
+    origin: 'right'
+});
+ScrollReveal().reveal('#titleContact', { 
+    delay: 400, 
+    distance: '50px',
+    duration: 1200,
+    origin: 'top'
+});
+ScrollReveal().reveal('#contact1', { 
+    delay: 600, 
+    distance: '50px',
+    duration: 1500,
+    origin: 'left'
+});
+ScrollReveal().reveal('#contact2', { 
+    delay: 600, 
+    distance: '50px',
+    duration: 1500,
+    origin: 'right'
+});
+
